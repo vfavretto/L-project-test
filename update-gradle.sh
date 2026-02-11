e0231fdc043c61a6de6c8a7337e182d1ef164698
Victor Favretto <143458351+vfavretto@users.noreply.github.com>
2026-02-10 22:14:21 -0300

    Delete update-gradle.sh

--- update-gradle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Atualizando Gradle Wrapper para versão 8.11.1..."
-./gradlew wrapper --gradle-version=8.11.1 --distribution-type=bin
-echo ""
-echo "Gradle Wrapper atualizado com sucesso!"
-echo "Execute './gradlew build' para testar."
