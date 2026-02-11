4d18862f89e0a4c8992e803ea0dd4a8af04ce202
Victor Favretto <143458351+vfavretto@users.noreply.github.com>
2026-02-10 22:14:02 -0300

    Delete update-gradle.bat

--- update-gradle.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Atualizando Gradle Wrapper para versao 8.11.1...
-gradlew.bat wrapper --gradle-version=8.11.1 --distribution-type=bin
-echo.
-echo Gradle Wrapper atualizado com sucesso!
-echo Execute 'gradlew.bat build' para testar.
-pause
